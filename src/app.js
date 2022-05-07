105b5645ab7f8a01af8c88f743f0095467fe923d
nilesh133 <nkpnilesh13@gmail.com>
2022-05-07 11:47:52 +0530

    commit

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+const urlMetadata = require('url-metadata')
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Hello");
+});
+
+app.get('/urlInfo/:id', async (req, res) => {
+    try {
+        const linkId = req.query.url;
+        console.log(req.query);
+        
+        urlMetadata(linkId).then(
+            function (metadata) {
+                res.status(201).send(metadata);
+            },
+            function (error) {
+                console.log(error)
+            })
+
+    } catch (error) {
+        res.status(404).send("Invalid URL");
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Successfully running at port ${port}`);
+})
+
+
+
+
+// Project Name: Url-Info
+// Cluster-name: url-info-cluster
+// username: urlinfo
+// password: urlinfopassword
+
+// mongodb+srv://urlinfo:<password>@url-info-cluster.4lvj9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+//f9b66f4f1baf40e2cde932366646c241
