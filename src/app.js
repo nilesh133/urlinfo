faa5543622a276534ff6f2605e1fe1babac7f357
nilesh133 <nkpnilesh13@gmail.com>
2022-05-07 14:02:12 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const urlMetadata = require('url-metadata')
+var cors = require('cors')
 
 app.use(express.json());
+
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.send("Hello");
